20aeecfdb88aff54360f38f586782ceacc475b9c
andrew@andreweast.net <andrew@andreweast.net>
2016-03-13 22:41:16 +0000

    Attempt to figure out the Twitch API

--- twitch.js
+++ twitch.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
 });
 
   // fetch Free Code Camp's Twitch channel data:
-	$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+  // https://github.com/justintv/Twitch-API
+  // https://github.com/justintv/Twitch-API/blob/master/v3_resources/streams.md#get-streamschannel
+  
+  var possibleStreams = ["riotgames", "playhearthstone", "freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+  
+  var apiURL = "https://api.twitch.tv/kraken/" +
+               "streams?" +
+               "channel=" + possibleStreams.join(",") + // freecodecamp,storbeck,terakilobyte" +
+               "&api_version=3&callback=?";
+  console.log(apiURL);
+	$.getJSON(apiURL, function(data) {
     console.log(data);
   });
