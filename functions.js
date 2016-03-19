b21c2968bc47c2979937755ff091fb08a99f4c98
andrew@andreweast.net <andrew@andreweast.net>
2016-03-19 20:32:49 +0000

    Complete more challenges!

--- functions.js
+++ functions.js
@@ -121,7 +121,7 @@ sumFibonacciNumbers(4);
 // *************************************************************
 function sumPrimes(max) {
   //find primes using Sieve of Eratosthenes:
-  var nonPrimes = Array(max); //array values empty; will mark composities true as found
+  var nonPrimes = Array(max); //array of same length as mas, all values are empty; will mark composities true as found
   var cachedSqrt = Math.sqrt(max); //from Eratosthenes: once we get to sqrt(max), all composities have been marked already
   for (var curr = 2; curr <= cachedSqrt; curr++) {
     //if curr is not already marked as non-prime (ie. composite):
@@ -173,5 +173,301 @@ function leastCommonMultiple(arr) {
 leastCommonMultiple([5,1]);
 
 // *************************************************************
+// **** Validate Telephone Number Using Finite State Machine *** 
+// *************************************************************
+
+String.prototype.isDigit = function() {
+  if (this[0] >= "0" && this[0] <= "9")
+    return true;
+  else
+    return false;
+};
+
+function telephoneCheck(str) {
+  // using a finite state machine:
+  var states = {
+    total: 6,
+    countryCode: true, // 1[ ]...
+    prefixParens: true, // ...(555)[ ]...
+    prefixDash: true, // ...555-555...
+    prefixSpace: true, // ...555 555...
+    
+    midDash: true, // ...555-5555
+    midSpace: true, // ...555 5555
+    
+    foundAreaCode: false, //(555)
+    foundTriplet: false, //555
+    foundQuartet: false, //5555
+    
+    foundOpenParens: false, // ...(...
+    foundPureNumbers: false, // ...5555555555
+    
+    foundSpaceAfterCountryCode: false //1 [(]555...
+  };
+  
+  var runLength = 0; //how many numbers in a row
+  
+  str = str.trim();
+  for (var i = 0; i < str.length; ++i) {
+    if (states.foundQuartet) { //&& area code && Triplet
+      states.total = 0; //there was more stuff after a completed, valid phone number
+    } else if (str[i].isDigit()) {
+      runLength++;
+      
+      if (i === 0 && str[i] !== "1") {
+        states.countryCode = false;
+        states.total--;
+      } else if (runLength > 4 && (states.foundAreaCode || states.foundTriplet || states.foundQuartet)) {
+        states.total = 0; //run length too long! short-circuit
+      } else if (runLength === 10) {
+        states.foundPureNumbers = true;
+        states.midDash = states.midSpace = false;
+        states.prefixDash = states.prefixParens = states.prefixSpace = false;
+        states.total = 1;
+      } else if (runLength > 10) {
+        states.total = 0;
+      }
+    } else { //not isDigit()
+      if (runLength === 3) {
+        if (!states.foundAreaCode) {
+          if (str[i] === ")") {
+            if (states.prefixParens && states.foundOpenParens) {
+              states.foundAreaCode = true;
+            } else {
+              states.total = 0; //there was no opening "("
+            }
+          } else if (str[i] === "-") {
+            if (states.prefixDash) {
+              states.prefixParens = states.prefixSpace = false;
+              states.total -= 2;
+              states.foundAreaCode = true;
+            } else {
+              states.total = 0; //got a dash when expecting ")"
+            }
+          } else if (str[i] === " ") {
+            if (states.prefixSpace) {
+              states.prefixDash = states.prefixParens = false;
+              states.total -= 2;
+              states.foundAreaCode = true;
+            } else {
+              states.total = 0; //was expecting a ")"
+            }
+          } else {
+            //the only valid chars following area code are /)- /
+            states.total = 0;
+          }
+        } else if (!states.foundTriplet) {
+          if (str[i] === "-") {
+            if (states.midDash) {
+              states.midSpace = false;
+              states.total--;
+              states.foundTriplet = true;
+            } else {
+              states.total = 0; //got a dash when expecting ")"
+            }
+          } else if (str[i] === " ") {
+            if (states.prefixDash) { //cannot have: 555-555 5555
+              states.total = 0;
+            } else if (states.midSpace) {
+              states.midDash = false;
+              states.total--;
+              states.foundTriplet = true;
+            }
+          } else {
+            //the only valid chars following Triplet are /- /
+            states.total = 0;
+          }
+        } else { //already found both area code and triplet!
+          states.total = 0;
+        }
+      //} else if (runLength === 4) {
+      //  if (!states.foundTriplet) { //&& !states.foundAreaCode
+      //    states.total = 0;
+      //  } else if (!states.foundQuartet) {
+      //    states.total = 0; //no characters are allowed after a quartet, except spaces, which are already .trim()'d
+      //  }
+      } else if (runLength === 1) {
+        if (states.foundAreaCode) {
+          states.total = 0; //country code no longer allowed
+        } else if (states.countryCode) {
+          if (str[i] === "(") {
+            states.foundOpenParens = true;
+            states.prefixSpace = states.prefixDash = false;
+            states.total -= 2;
+          } else if (str[i] === " ") {
+            //a space doesn't inform anything about prefix
+            states.foundSpaceAfterCountryCode = true;
+          }
+        } else { //only a country code can be runLength = 1
+          states.total = 0;
+        }
+      } else if (runLength === 0) {
+        if (i === 0 && str[i] === "(") {
+          states.countryCode = false;
+          states.foundOpenParens = true;
+          states.prefixSpace = states.prefixDash = states.pureNumbers = false;
+          states.total -= 3;
+        } else if (!states.foundAreaCode && states.foundSpaceAfterCountryCode) {
+          states.foundSpaceAfterCountryCode = false; //can't find twice
+          if (str[i] === "(") {
+            states.foundOpenParens = true;
+            states.prefixSpace = states.prefixDash = states.pureNumbers = false;
+            states.total -= 3;
+          } else { //can ONLY be "1 (..."
+            states.total = 0;
+          }
+        } else if (str[i] === " " ) {
+          if (i > 0 && str[i - 1] === ")" && states.foundAreaCode && !states.foundTriplet) {
+            //space after area code paren: "...555) ..."
+          } else {
+            states.total = 0; //format disallows double spaces...
+          }
+        } else {
+          states.total = 0; //no other valid double punctuation
+        }
+      } else { //TODO: are there any other valid run lengths?
+        states.total = 0;
+      }
+      
+      runLength = 0;
+    }
+    
+    if (states.total <= 0)
+      return false;
+  }
+  
+  if (states.foundQuartet) {
+    return true;
+  } else if (states.foundPureNumbers) {
+    return true;
+  } else {
+    if (runLength === 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  //go to the end of the string, with at least one state valid
+  //return true;
+}
+
+
+telephoneCheck("1 (555) 555-5555");
+// telephoneCheck("555-555-5555") should return a boolean.
+// telephoneCheck("1 555-555-5555") should return true.
+// telephoneCheck("1 (555) 555-5555") should return true.
+// telephoneCheck("5555555555") should return true.
+// telephoneCheck("555-555-5555") should return true.
+// telephoneCheck("(555)555-5555") should return true.
+// telephoneCheck("1(555)555-5555") should return true.
+// telephoneCheck("1 555)555-5555") should return false.
+// telephoneCheck("1 555 555 5555") should return true.
+// telephoneCheck("1 456 789 4444") should return true.
+// telephoneCheck("123**&!!asdf#") should return false.
+// telephoneCheck("55555555") should return false.
+// telephoneCheck("(6505552368)") should return false
+// telephoneCheck("2 (757) 622-7382") should return false.
+// telephoneCheck("0 (757) 622-7382") should return false.
+// telephoneCheck("-1 (757) 622-7382") should return false
+// telephoneCheck("2 757 622-7382") should return false.
+// telephoneCheck("10 (757) 622-7382") should return false.
+// telephoneCheck("27576227382") should return false.
+// telephoneCheck("(275)76227382") should return false.
+// telephoneCheck("2(757)6227382") should return false.
+// telephoneCheck("2(757)622-7382") should return false.
+// telephoneCheck("555)-555-5555") should return false.
+// telephoneCheck("(555-555-5555") should return false.
+
+
+// *************************************************************************
+// *** Set Theory: Union, Intersection, Difference, Symmetric Difference ***
+// *************************************************************************
+function sym(args) {
+  if (arguments.length > 0) {
+    //console.log("args[0]: " + arguments[0]);
+    var current = arguments[0];
+    for (var i = 1; i < arguments.length; ++i) {
+      //console.log("args["+i+"]: " + arguments[i]);
+      //definition of symetric difference: https://en.wikipedia.org/wiki/Symmetric_difference
+      //sym diff = union minus intersection
+      current = complement(union(current, arguments[i]), intersection(current, arguments[i]));
+      //console.log("result: " + current);
+    }
+    return current;
+  } else { //no arguments
+    return [];
+  }
+}
+
+
+//function: complement(...)
+// takes two Arrays, first the master, second the set to "subtract" from the master
+// returns the complement/difference (set theory) of the two arrays
+function complement(master, remove) {
+  var markToRemove = Array(master.length); //don't have to initialize array values, since we will only check for positive correlation later, not either value
+  for (var i = 0; i < remove.length; ++i) {
+    var found = 0;
+    while ((found = master.indexOf(remove[i], found)) !== -1) {
+      markToRemove[found] = true;
+      found++;
+    }
+  }
+  //console.log("markToRemove:" + markToRemove);
+  var reduced = [];
+  for (i = 0; i < markToRemove.length; ++i) {
+    if (!markToRemove[i]) {
+      reduced.push(master[i]);
+    }
+  }
+  return reduced;
+}
+
+//function: intersection(...)
+// takes a variable length argument list of Arrays
+// returns the intersection (set theory) of all those arrays
+function intersection(args) {
+  var intersect = arguments[0];
+  for (var i = 1; i < arguments.length; ++i) {
+    var tempArray = [];
+    for (var j = 0; j < arguments[i].length; ++j) {
+      if (intersect.indexOf(arguments[i][j]) !== -1 && tempArray.indexOf(arguments[i][j]) === -1) {
+          tempArray.push(arguments[i][j]);
+      }
+    }
+    intersect = tempArray.slice(0); //shallow clone array
+  }
+  return intersect;
+}
+
+//function: union(...)
+// takes a variable length argument list of Arrays
+// returns the union (set theory) of all those arrays
+function union(args) {
+  var combined = [];
+  for (var i = 0; i < arguments.length; ++i) {
+    combined = combined.concat(arguments[i]);
+  }
+  var reduced = [];
+  for (i = 0; i < combined.length; ++i) {
+    if (reduced.indexOf(combined[i]) === -1) {
+      reduced.push(combined[i]);
+    }
+  }
+  return reduced;
+}
+
+
+//symettric diff: A delta B === (A union B) minus (A intersection B)
+console.log("**** START ****");
+//console.log("sym([1, 2, 3], [5, 2, 1, 4])");
+//console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+console.log("sym([3,3,3,2,5], [2,1,5,7])");
+console.log(sym([3,3,3,2,5], [2,1,5,7]));
+console.log("***************");
+
+
 // *************************************************************
 // *************************************************************
+// *************************************************************
+
