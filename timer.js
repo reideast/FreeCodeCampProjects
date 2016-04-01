4a9584d8c26420129652a6aa3a1dcb578c609ef7
andrew@andreweast.net <andrew@andreweast.net>
2016-04-01 21:31:41 +0100

    Created and styled values for work/break timers. Implementing generic "spinner" class for buttons.

--- timer.js
+++ timer.js
@@ -1,34 +1,47 @@
 var DEBUG = true;
 
 $(document).ready(function () {
+  
   resetTimer();
+  
   $("#btnStart").on("click", function() {
     if (intervalID === undefined) {
       intervalID = window.setInterval(updateTimer, 1000);
-      $("#btnStart").html('<i class="fa fa-2x fa-pause"></i>');
+      $("#btnStart").html('<i class="fa fa-2x fa-fw fa-pause"></i>');
     } else {
       window.clearInterval(intervalID);
-      $("#btnStart").html('<i class="fa fa-2x fa-play"></i>');
+      $("#btnStart").html('<i class="fa fa-2x fa-fw fa-play"></i>');
       intervalID = undefined;
     }
   });
   
   $("#btnReset").on("click", function() {
-    $("#btnStart").html('<i class="fa fa-2x fa-play"></i>');
+    $("#btnStart").html('<i class="fa fa-2x fa-fw fa-play"></i>');
     if (intervalID !== undefined) {
       window.clearInterval(intervalID);
       intervalID = undefined;
     }
     resetTimer();
   });
+  
+  $(".btn-spinner").on("click", function() {
+    
+  });
+  // data-linked-control="#workValue" data-spinner-delta="1"
 });
 
-var WORK = 1 * 60; //25 min
-var BREAK = 1 * 60; //5 min
+var WORK = 25 * 60; //25 min
+var BREAK = 5 * 60; //5 min
 var currWork = WORK;
 var currBreak = BREAK;
 var working = true;
 var intervalID;
+
+//see: http://stackoverflow.com/questions/9419263/playing-audio-with-javascript
+var chimeWorkDone = new Audio('http://dev.andreweast.net/FreeCodeCamp/chimeToBreak Zen Buddhist Temple Bell-SoundBible.com-331362457.mp3');
+// "Zen Buddhist Temple Bell Sound" Recorded by Mike Koenig, from http://soundbible.com/1491-Zen-Buddhist-Temple-Bell.html, licensed under Creative Commons Attribution 3.0 https://creativecommons.org/licenses/by/3.0/
+var chimeBreakDone = new Audio('http://dev.andreweast.net/FreeCodeCamp/chimeToWork Japanese Temple Bell Small-SoundBible.com-113624364.mp3');
+// "Japanese Temple Bell Small Sound" Recorded by Mike Koenig, from http://soundbible.com/1496-Japanese-Temple-Bell-Small.html, licensed under Creative Commons Attribution 3.0 https://creativecommons.org/licenses/by/3.0/
 
 function resetTimer() {
   currWork = WORK;
@@ -37,7 +50,6 @@ function resetTimer() {
   resetAnalog();
   updateDigital();
   updateAnalog();
-  
 }
 function resetAnalog() {
   $("#barWorkContainer").css("width", "calc(" + WORK + " / " + (WORK + BREAK) + " * 100%)");
@@ -70,13 +82,10 @@ function updateTimer() {
 }
 
 function eventWorkIsDone() {
-  
+  chimeWorkDone.play();
 }
 function eventBreakIsDone() {
-  
-}
-function verbalAlarm() {
-  
+  chimeBreakDone.play();
 }
 
 function updateDigital() {
