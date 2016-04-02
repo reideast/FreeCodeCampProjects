6d736bb494701f7845b15676389f1b600d43b222
andrew@andreweast.net <andrew@andreweast.net>
2016-04-02 23:40:40 +0100

    Finished Number input "spinner" javascript. Cleaned up, commented.

--- timer.js
+++ timer.js
@@ -1,35 +1,4 @@
-var DEBUG = true;
-
-$(document).ready(function () {
-  
-  resetTimer();
-  
-  $("#btnStart").on("click", function() {
-    if (intervalID === undefined) {
-      intervalID = window.setInterval(updateTimer, 1000);
-      $("#btnStart").html('<i class="fa fa-2x fa-fw fa-pause"></i>');
-    } else {
-      window.clearInterval(intervalID);
-      $("#btnStart").html('<i class="fa fa-2x fa-fw fa-play"></i>');
-      intervalID = undefined;
-    }
-  });
-  
-  $("#btnReset").on("click", function() {
-    $("#btnStart").html('<i class="fa fa-2x fa-fw fa-play"></i>');
-    if (intervalID !== undefined) {
-      window.clearInterval(intervalID);
-      intervalID = undefined;
-    }
-    resetTimer();
-  });
-  
-  $(".btn-spinner").on("click", function() {
-    
-  });
-  // data-linked-control="#workValue" data-spinner-delta="1"
-});
-
+var DEBUG = false;
 var WORK = 25 * 60; //25 min
 var BREAK = 5 * 60; //5 min
 var currWork = WORK;
@@ -42,6 +11,57 @@ var chimeWorkDone = new Audio('http://dev.andreweast.net/FreeCodeCamp/chimeToBre
 // "Zen Buddhist Temple Bell Sound" Recorded by Mike Koenig, from http://soundbible.com/1491-Zen-Buddhist-Temple-Bell.html, licensed under Creative Commons Attribution 3.0 https://creativecommons.org/licenses/by/3.0/
 var chimeBreakDone = new Audio('http://dev.andreweast.net/FreeCodeCamp/chimeToWork Japanese Temple Bell Small-SoundBible.com-113624364.mp3');
 // "Japanese Temple Bell Small Sound" Recorded by Mike Koenig, from http://soundbible.com/1496-Japanese-Temple-Bell-Small.html, licensed under Creative Commons Attribution 3.0 https://creativecommons.org/licenses/by/3.0/
+
+
+$(document).ready(function () {
+  
+  resetTimer();
+  
+  $("#btnStart").on("click", function() {
+    if (intervalID === undefined) {
+      intervalID = window.setInterval(updateTimer, 1000);
+      $("#btnStart").html('<i class="fa fa-2x fa-fw fa-pause"></i>');
+      toggleModifiers(false);
+    } else {
+      window.clearInterval(intervalID);
+      $("#btnStart").html('<i class="fa fa-2x fa-fw fa-play"></i>');
+      intervalID = undefined;
+      toggleModifiers(true);
+    }
+  });
+  
+  $("#btnReset").on("click", function() {
+    $("#btnStart").html('<i class="fa fa-2x fa-fw fa-play"></i>');
+    if (intervalID !== undefined) {
+      window.clearInterval(intervalID);
+      intervalID = undefined;
+    }
+    toggleModifiers(true);
+    resetTimer();
+  });
+  
+  $(".btn-spinner").on("click", function() {
+    var linkedControl = $($(this).data("linked-control"));
+    linkedControl.text(parseInt(linkedControl.text()) + parseInt($(this).data("spinner-delta")));
+    
+    //found through testing that reducing a time to 0 breaks the design
+    if (linkedControl.text() <= 0) {
+      linkedControl.text(1);
+    }
+    //hard coding this event here only because my idea to separately bind an onChange event wouldn't work
+    if ($(this).data("linked-control") === "#workValue") {
+      WORK = linkedControl.text() * 60;
+    } else if ($(this).data("linked-control") === "#breakValue") {
+      BREAK = linkedControl.text() * 60;
+    }
+    resetTimer();
+  });
+  //nevermind, onChange only works on <input> elem
+  // $("#workValue").on("change", function () {
+  //   console.log("onchange: " + $(this).text());
+  // });
+});
+
 
 function resetTimer() {
   currWork = WORK;
@@ -65,7 +85,7 @@ function updateTimer() {
       currWork = 0;
       currBreak = BREAK;
       working = false;
-      eventWorkIsDone();
+      workIsDone();
     }
   } else {
     --currBreak;
@@ -74,17 +94,17 @@ function updateTimer() {
       currBreak = 0;
       currWork = WORK;
       working = true;
-      eventBreakIsDone();
+      breakIsDone();
     }
   }
   updateDigital();
   updateAnalog();
 }
 
-function eventWorkIsDone() {
+function workIsDone() {
   chimeWorkDone.play();
 }
-function eventBreakIsDone() {
+function breakIsDone() {
   chimeBreakDone.play();
 }
 
@@ -97,6 +117,11 @@ function updateDigital() {
 function updateAnalog() {
   $("#barWork").css("width", (currWork / WORK) * 100 + "%");
   $("#barBreak").css("width", (currBreak / BREAK) * 100 + "%");
+  //animation is "twitchy" in Chrome, so ditching it
   // $("#barWork").animate({width: (currWork / WORK) * 100 + "%"}, 1000);
   // $("#barBreak").animate({width: (currBreak / BREAK) * 100 + "%"}, 1000);
+}
+
+function toggleModifiers(isVisible) {
+  $(".barValue").toggle(isVisible);
 }
