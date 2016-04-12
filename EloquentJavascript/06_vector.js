866a2e3b36e1d5c12e5a331c5f0be4ef590baa31
Andrew Reid East <andrew@andreweast.net>
2016-04-12 19:22:05 +0100

    Eloquent Javascript 06 vector

---
+++ EloquentJavascript/06_vector.js
@@ -0,0 +1,23 @@
+// Your code here.
+function Vector(newX, newY) {
+  this.x = newX;
+  this.y = newY;
+}
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+});
+Vector.prototype.plus = function(vector2) {
+  return new Vector(this.x + vector2.x, this.y + vector2.y);
+};
+Vector.prototype.minus = function(vector2) {
+  return new Vector(this.x - vector2.x, this.y - vector2.y);
+};
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
