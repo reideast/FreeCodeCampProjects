a90b4bcda701476848c26ddeddf5e8b3e0355983
Andrew Reid East <andrew@andreweast.net>
2016-04-13 21:34:14 +0100

    Eloquent Javascript 06 iterator

---
+++ EloquentJavascript/06_iterate.js
@@ -0,0 +1,71 @@
+// Your code here.
+function Iterator() {
+  this.pointer = 0;
+  this.obj = undefined;
+  //allows starting the iterator over at the beginning
+  this.reset = function() {
+    return undefined;
+  }
+  //returns the next item in the sequence, or "undefined" if past the end of the sequence
+  //what happens if an item in the sequence is actually equal to "null", "undefined", or "false"?
+  this.getNext = function() {
+    return undefined;
+  }
+}
+
+function ArraySeq(array) {
+  this.obj = array;
+  this.pointer = -1;
+}
+ArraySeq.prototype = Object.create(Iterator.prototype);
+ArraySeq.prototype.reset = function() {
+  this.pointer = -1;
+}
+ArraySeq.prototype.getNext = function() {
+  this.pointer++;
+  if (this.pointer < this.obj.length) {
+    return this.obj[this.pointer];
+  } else {
+    return undefined;
+  }
+}
+
+function RangeSeq(min, max) {
+  this.min = min;
+  this.max = max;
+  this.pointer = min;
+}
+RangeSeq.prototype = Object.create(Iterator.prototype);
+RangeSeq.prototype.reset = function() {
+  this.pointer = this.min;
+}
+RangeSeq.prototype.getNext = function() {
+  if (this.pointer <= this.max) {
+    return this.pointer++;
+  } else {
+    return undefined;
+  }
+}
+
+function logFive(iterator) {
+  iterator.reset();
+  var count = 0;
+  var curr = undefined;
+  while (count++ < 5 && (curr = iterator.getNext()) !== undefined) {
+    console.log(curr);
+  }
+}
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new ArraySeq([1, false, 2]));
+logFive(new ArraySeq([1, null, 2]));
+logFive(new ArraySeq([1, undefined, 2]));
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
+logFive(new RangeSeq(100, 102));
