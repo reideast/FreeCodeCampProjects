f2448d88957128dc383c2457a854fedeef929c4b
Andrew Reid East <andrew@andreweast.net>
2016-04-16 21:06:06 +0100

    Install and set up Jasmine testing framework

--- EloquentJavascript/06_iterate.js
+++ EloquentJavascript/06_iterate.js
@@ -56,16 +56,16 @@ function logFive(iterator) {
   }
 }
 
-logFive(new ArraySeq([1, 2]));
-// → 1
-// → 2
-logFive(new ArraySeq([1, false, 2]));
-logFive(new ArraySeq([1, null, 2]));
-logFive(new ArraySeq([1, undefined, 2]));
-logFive(new RangeSeq(100, 1000));
-// → 100
-// → 101
-// → 102
-// → 103
-// → 104
-logFive(new RangeSeq(100, 102));
+// logFive(new ArraySeq([1, 2]));
+// // → 1
+// // → 2
+// logFive(new ArraySeq([1, false, 2]));
+// logFive(new ArraySeq([1, null, 2]));
+// logFive(new ArraySeq([1, undefined, 2]));
+// logFive(new RangeSeq(100, 1000));
+// // → 100
+// // → 101
+// // → 102
+// // → 103
+// // → 104
+// logFive(new RangeSeq(100, 102));
