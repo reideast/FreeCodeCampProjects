5845a043df7d21d586f3716b5f86528d1b54d1bb
Andrew Reid East <andrew@andreweast.net>
2016-04-19 10:08:43 +0100

    Eloquent Javsascript 10 simple module

---
+++ EloquentJavascript/10_monthModule.js
@@ -0,0 +1,14 @@
+// Your code here.
+var month = function() {
+  var names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return {
+    name: function(num) { return names[num] },
+    number: function(name) {return names.indexOf(name); }
+  };
+}();
+
+
+console.log(month.name(2));
+// → March
+console.log(month.number("November"));
+// → 10
