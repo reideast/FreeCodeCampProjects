c151734ff47e0a71136122a1cafb39ffe24d0494
Andrew Reid East <andrew@andreweast.net>
2016-04-25 23:03:21 +0100

    Create event handler register clicks on spaces (simply cycles x/o for now)

--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,52 @@
+var board = new Board();
+
+
 $(document).ready(function() {
   $(".space").addClass("squareContent").before('<div class="squareDummy"></div>');
+  $(".space").click(spaceClicked);
   $(window).resize(syncFontSize); 
-  syncFontSize();
   
-	var board = new Board();
-	board.setSpace(2, true);
-	board.setSpace(4, false);
-	board.setSpace(5, true);
 	board.showDebug();
+  syncFontSize();
 });
-
 function syncFontSize() {
   $(".space>span").css("font-size", ($(".space").height() * 0.85) + "px");
 }
 
+function spaceClicked() {
+  var space = $(this).data("space");
+  // console.log("Clicked #" + space);
+  if (board.isEmpty(space))
+    board.setPlayer(space);
+  else if (board.getSpace(space) == -1)
+    board.setComp(space);
+  else
+    board.clearSpace(space);
+  syncBoard();
+	board.showDebug();
+}
+function syncBoard() {
+  $(".space").each(function(i) {
+    var val = board.getSpace(this.dataset.space);
+    var className;
+    if (val == 0)
+      className = "";
+    else if (val == -1)
+      className = "fa fa-times";
+    else if (val == 1)
+      className = "fa fa-circle-o";
+    this.firstChild.className = className;
+  });
+}
+
 function Board() {
+  // idea for this tic-tac-toe logic:
+  // keep track of totals for all winning rows/cols/diags
+  //   computer is 1, player is -3
+  //   rows where the human has played anything are always negative, and the compute has no chance of winning
+  //   rows which the human has to be blocked are total < -3
+  //   rows where the computer has a chance of winning are higher
+  // therefore, chose from totals by number: first, block a human win. then, chose any row/col/diag by highest first
 	var board = [0,0,0, 0,0,0, 0,0,0]; // 0 for blank, 1 for computer, -3 for player
 	var totals = [0,0,0, 0,0,0, 0,0];
 	//var totals = {
@@ -42,10 +74,14 @@ function Board() {
 		}
 	};
 	
-	this.setSpace = function(space, isPlayer) {
+  this.setPlayer = function(space) { setSpace(space, -3); };
+  this.setComp = function(space) { setSpace(space, 1); };
+  this.clearSpace = function(space) { setSpace(space, 0); };
+  function setSpace(space, val) {
 		if (space >= 0 && space <= 8) {
-			board[space] = isPlayer ? -3 : 1;
-			this.calcTotals();
+			// board[space] = isPlayer ? -3 : 1;
+      board[space] = val;
+			calcTotals();
 		} else {
 			console.log("Tried to set space with invalid space number: " + space);
 		}
@@ -56,7 +92,7 @@ function Board() {
 				return 1;
 			else if (board[space] === -3)
 				return -1;
-			else if (board[space === 0])
+			else if (board[space] === 0)
 				return 0;
 			else
 				console.log("Got space # " + space + " with invalid value: " + board[space]);
@@ -68,7 +104,7 @@ function Board() {
 		return this.getSpace(space) === 0;
 	};
 	
-	this.calcTotals = function() {
+	function calcTotals() {
 		totals[0] = board[0] + board[1] + board[2]; // rows[0]
 		totals[1] = board[3] + board[4] + board[5]; // rows[1]
 		totals[2] = board[6] + board[7] + board[8]; // rows[2]
