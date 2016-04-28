7ff282189838510b36e52628d488f089ecc73e37
Andrew Reid East <andrew@andreweast.net>
2016-04-28 21:40:31 +0100

    Creating node.js server from Eloquent Javascript

---
+++ EloquentJavascript/server/router.js
@@ -0,0 +1,26 @@
+var Router = modules.export = function() {
+  this.routes = [];
+};
+
+Router.prototype.add = function(method, url, handler) {
+  this.routes.push({
+    method: method,
+    url: url,
+    handler: handler
+  });
+};
+
+Router.prototype.resolve = function(request, response) {
+  var path = require("url").parse(request.url).pathname;
+  
+  return this.routes.some(function(route) { // Array.some() runs through elements one at a time, and will stop after function returns true (short circuit, like || operator does)
+    var match = route.url.exec(path);
+    if (!match || route.method != request.method)
+      return false;
+      
+    var urlParts = match.slice(1).map(decodeURIcomponent);
+    route.handler.apply(null, [request, response].concat(urlParts));
+    
+    return true;
+  })
+};
